aae62e1847b5ce56fb060772329045d8916cc695
Andrei Shuvalov <6286552+dronbas@users.noreply.github.com>
2025-12-09 22:53:35 +0100

    Fix guarded

--- shared/src/traits/guarded.rs
+++ shared/src/traits/guarded.rs
@@ -1,8 +1,8 @@
 use async_trait::async_trait;
 use tokio::sync::OwnedMutexGuard;
 
-#[cfg(feature = "wasm")]
-#[async_trait(?Send)]
+#[cfg_attr(feature = "wasm", async_trait(?Send))]
+#[cfg_attr(not(feature = "wasm"), async_trait)]
 pub trait AsyncGuarded<Inner> {
     async fn async_guarded<F, T>(&self, action: F) -> T
     where
@@ -10,31 +10,12 @@ pub trait AsyncGuarded<Inner> {
         T: 'async_trait;
 }
 
-#[cfg(not(feature = "wasm"))]
-#[async_trait]
-pub trait AsyncGuarded<Inner> {
-    async fn async_guarded<F, T>(&self, action: F) -> T
-    where
-        F: FnOnce(&Inner) -> T + Send + 'async_trait,
-        T: Send + 'async_trait;
-}
-
-#[cfg(feature = "wasm")]
-#[async_trait(?Send)]
+#[cfg_attr(feature = "wasm", async_trait(?Send))]
+#[cfg_attr(not(feature = "wasm"), async_trait)]
 pub trait AsyncGuardedMut<Inner> {
     async fn async_guarded_mut<F, Fut, T>(&self, action: F) -> anyhow::Result<T>
     where
         F: FnOnce(OwnedMutexGuard<Inner>) -> Fut + 'async_trait,
         Fut: Future<Output = anyhow::Result<T>> + 'async_trait,
         T: 'async_trait;
-}
-
-#[cfg(not(feature = "wasm"))]
-#[async_trait]
-pub trait AsyncGuardedMut<Inner> {
-    async fn async_guarded_mut<F, Fut, T>(&self, action: F) -> anyhow::Result<T>
-    where
-        F: FnOnce(OwnedMutexGuard<Inner>) -> Fut + Send + 'async_trait,
-        Fut: Future<Output = anyhow::Result<T>> + Send + 'async_trait,
-        T: Send + 'async_trait;
 }
